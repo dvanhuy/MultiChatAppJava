38ea1470b48b1549a2a0e478a1f1a48ba0ea8712
unknown <huydinhvan13@gmail.com>
2023-04-05 22:52:57 +0700

    final

--- src/ServerGUI/ServerUI.java
+++ src/ServerGUI/ServerUI.java
@@ -185,6 +185,7 @@ public class ServerUI extends javax.swing.JFrame {
         jLabel2 = new javax.swing.JLabel();
         txtnumberclient = new javax.swing.JLabel();
         txtnumberid = new javax.swing.JLabel();
+        btlistclient = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -259,6 +260,13 @@ public class ServerUI extends javax.swing.JFrame {
         txtnumberid.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
         txtnumberid.setText(" ");
 
+        btlistclient.setText("Xem danh sách client");
+        btlistclient.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btlistclientActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -266,19 +274,25 @@ public class ServerUI extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, 281, Short.MAX_VALUE)
                     .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 128, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGap(18, 18, 18)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(txtnumberclient, javax.swing.GroupLayout.PREFERRED_SIZE, 91, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(txtnumberid, javax.swing.GroupLayout.PREFERRED_SIZE, 91, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGap(0, 0, Short.MAX_VALUE)))
-                .addContainerGap())
+                            .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, 281, Short.MAX_VALUE)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                    .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                    .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 128, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addGap(18, 18, 18)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(txtnumberclient, javax.swing.GroupLayout.PREFERRED_SIZE, 91, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(txtnumberid, javax.swing.GroupLayout.PREFERRED_SIZE, 91, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addGap(0, 0, Short.MAX_VALUE)))
+                        .addContainerGap())
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(63, 63, 63)
+                        .addComponent(btlistclient, javax.swing.GroupLayout.PREFERRED_SIZE, 149, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -297,7 +311,9 @@ public class ServerUI extends javax.swing.JFrame {
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(jLabel2)
-                            .addComponent(txtnumberid))))
+                            .addComponent(txtnumberid))
+                        .addGap(18, 18, 18)
+                        .addComponent(btlistclient, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
@@ -341,7 +357,6 @@ public class ServerUI extends javax.swing.JFrame {
             listServer.remove(serverDefinetemp);
             updateListRoom();
         }
-        
     }//GEN-LAST:event_btRemoveRoomActionPerformed
 
     private void listRoomValueChanged(javax.swing.event.ListSelectionEvent evt) {//GEN-FIRST:event_listRoomValueChanged
@@ -353,6 +368,21 @@ public class ServerUI extends javax.swing.JFrame {
             }
         }
     }//GEN-LAST:event_listRoomValueChanged
+
+    private void btlistclientActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btlistclientActionPerformed
+        // TODO add your handling code here:
+        DefaultListModel<String> modelList = new DefaultListModel<>(); 
+        for (ServerDefine serverDefine : listServer) {
+            if (serverDefine.getServerSocket().getLocalPort()==Integer.parseInt(listRoom.getSelectedValue()))
+            {
+                for (ClientDefine cd : serverDefine.getClientList()) {
+                    modelList.addElement(cd.getClientName());
+                }
+            }
+        }
+        ListClient client = new ListClient(modelList);
+        client.setVisible(true);
+    }//GEN-LAST:event_btlistclientActionPerformed
 
     /**
      * @param args the command line arguments
@@ -392,6 +422,7 @@ public class ServerUI extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btAddRoom;
     private javax.swing.JButton btRemoveRoom;
+    private javax.swing.JButton btlistclient;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
